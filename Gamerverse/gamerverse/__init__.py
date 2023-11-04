834be1c70ec50c6884a402945941813cdf0aa953
Nathan Grilliot <116453721+nrgrill@users.noreply.github.com>
2023-11-04 14:48:21 -0400

    Added files

---
+++ Gamerverse/gamerverse/__init__.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+from gamerverse.config import Config
+
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+mail = Mail()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from gamerverse.users.routes import users
+    from gamerverse.posts.routes import posts
+    from gamerverse.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+    with app.app_context():
+        db.create_all()
+
+    return app
